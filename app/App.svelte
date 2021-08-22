2d7db4d80b1dfab99c3a9995329fad18a353d266
Ejiro Asiuwhu <ejiroasiuwhu10@gmail.com>
2021-08-22 18:18:15 +0100

    user details component

--- app/App.svelte
+++ app/App.svelte
@@ -1,38 +1,53 @@
 <script lang="javascript">
   import { onMount } from "svelte";
   import { Template } from "svelte-native/components";
+  import Details from "./pages/Details.svelte";
+  import { navigate } from "svelte-native";
 
   let users = [];
 
   const getUsers = async () => {
     try {
-      let res = await fetch("https://randomuser.me/api/?results=30");
-      let { results } = await res.json();
-      users = results;
-      isReady = true;
+      let res = await fetch("https://jsonplaceholder.typicode.com/users");
+      let data = await res.json();
+      users = data;
     } catch (err) {
       console.log(err);
     }
   };
 
-  function onItemTap() {
-    console.log("tap");
+  function onItemTap(args) {
+    // console.log(
+    //   `Item ${users[args.index].id} at index: ${args.index} was tapped`
+    // );
+    navigate({
+      page: Details,
+      props: { user: users[args.index] },
+    });
+    // navigate({ page: Details });
   }
 
   onMount(async () => {
     await getUsers();
-    users.forEach((user) => console.log(user.email));
   });
 </script>
 
 <page>
-  <actionBar title="Svelte Native App" />
+  <actionBar title="List of Users" />
+  <button text="To Details directly" on:tap={onItemTap} />
+
   <flexboxLayout>
     <listView items={users} on:itemTap={onItemTap} row="1" colSpan="2">
       <Template let:item>
         <flexboxLayout>
-          <label text={item.name.first} textWrap="true" class="first" />
-          <label text={item.name.last} textWrap="true" class="last" />
+          <label text={item.name} textWrap="true" class="name" />
+        </flexboxLayout>
+        <flexboxLayout>
+          <label text={"@" + item.username} textWrap="true" class="last" />
+        </flexboxLayout>
+        <flexboxLayout>
+          <label text="Email:" textWrap="true" class="first" />
+          <label text={item.email} textWrap="true" class="first" />
         </flexboxLayout>
       </Template>
     </listView>
---
+++ app/pages/Details.svelte
@@ -0,0 +1,53 @@
+<script>
+  import { onMount } from "svelte";
+
+  export let user;
+
+  function onItemTap() {
+    console.log(`User ${user}`);
+
+    // navigate({ page: Details });
+  }
+  onMount(() => {
+    console.log(user);
+  });
+</script>
+
+<page>
+  <stackLayout>
+    <flexboxLayout>
+      <flexboxLayout>
+        <label text={user.name} textWrap="true" class="name" />
+      </flexboxLayout>
+      <flexboxLayout>
+        <label
+          text={"@" + user.username.toLowerCase()}
+          textWrap="true"
+          class="last"
+        />
+      </flexboxLayout>
+      <flexboxLayout>
+        <label text="Email:" textWrap="true" class="first" />
+        <label text={user.email} textWrap="true" class="first" />
+      </flexboxLayout>
+      <label text={user.address.city} textWrap="true" class="first" />
+      <label text={user.address.street} textWrap="true" class="first" />
+      <label text={user.phone} textWrap="true" class="first" />
+    </flexboxLayout>
+  </stackLayout>
+</page>
+
+<style>
+  StackLayout {
+    width: 300;
+    height: 300;
+    padding: 10 16;
+    background-color: #f0f0f0;
+    box-shadow: 0 10px 53px 0 red;
+    color: #4831d4;
+  }
+  label {
+    font-size: 18;
+    margin: 8 0;
+  }
+</style>
